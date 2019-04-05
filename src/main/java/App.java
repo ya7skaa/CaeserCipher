60ae95592aafc77c9d04ceda881967a8a62d85ed
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-04-05 18:39:00 +0300

    asked for user to imput their story

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main (String [] args){
+        Scanner myConsole = new Scanner(System.in);
+        System.out.println("What is your story?");
+
 
     }
 
