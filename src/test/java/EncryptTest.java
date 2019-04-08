0af6a0efef578ca383034dc7e52608834698a620
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-04-08 17:40:57 +0300

    ädd testing

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -5,7 +5,7 @@ import static junit.framework.TestCase.assertEquals;
 
 public class EncryptTest {
     @Test
-    public void encrypter_testEncrypter_StringBuilder(){
+    public void encrypter_testEncrypter_String(){
         Encrypt encryptTest = new Encrypt();
         String expectedOutput = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
         assertEquals(expectedOutput, encryptTest.encrypter("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 23));
