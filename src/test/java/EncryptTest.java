331732ad6564b53007d1c8ead0119294e4be2864
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-04-08 17:50:01 +0300

    testing for lowercase story

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -7,8 +7,8 @@ public class EncryptTest {
     @Test
     public void encrypter_testEncrypter_String(){
         Encrypt encryptTest = new Encrypt();
-        String expectedOutput = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
-        assertEquals(expectedOutput, encryptTest.encrypter("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 23));
+        String expectedOutput = "bcd";
+        assertEquals(expectedOutput, encryptTest.encrypter("abc", 1));
 
     }
 
