2b1680546139f949182aa944b78768c89de04609
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-04-08 17:51:25 +0300

    testing for uppercase story

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -5,6 +5,13 @@ import static junit.framework.TestCase.assertEquals;
 
 public class EncryptTest {
     @Test
+    public void encrypter_testUpperCase_String(){
+        Encrypt encryptTest = new Encrypt();
+        String expectedOutput = "BCD";
+        assertEquals(expectedOutput, encryptTest.encrypter("ABC", 1));
+
+    }
+    @Test
     public void encrypter_testEncrypter_String(){
         Encrypt encryptTest = new Encrypt();
         String expectedOutput = "bcd";
