755df8b6f828472a8b72633a9cf4aedc2fdc7d75
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-04-08 08:14:15 +0300

    created new string builder decrpyt instance

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,7 @@
+package CeaserCipher;
+
 public class Decrypt {
+    public static String Builder decrypter(String story, int shift){
+        StringBuilder dncResult = new StringBuilder();
+    }
 }
